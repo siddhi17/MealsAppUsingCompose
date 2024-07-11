3331747542b7330ccf9485f8ef7f72387fdaae66
siddhi17 <siddhijambhale@gmail.com>
2024-07-11 11:04:25 +0530

    Using Coroutines in Compose

--- app/src/main/java/com/example/mealsappusingcompose/meals/MainActivity.kt
+++ app/src/main/java/com/example/mealsappusingcompose/meals/MainActivity.kt
@@ -7,13 +7,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mealsappusingcompose.model.response.MealResponse
 import com.example.mealsappusingcompose.ui.theme.MealsAppUsingComposeTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -30,11 +34,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MealsCategoriesScreen() {
     val viewModel: MealsCategoriesViewModel = viewModel()
-    val rememberedMeals: MutableState<List<MealResponse>> = remember { mutableStateOf(emptyList<MealResponse>()) }
-    viewModel.getMeals { response ->
-        val mealsFromTheApi = response?.categories
-       rememberedMeals.value = mealsFromTheApi.orEmpty()
+    val rememberedMeals: MutableState<List<MealResponse>> =
+        remember { mutableStateOf(emptyList<MealResponse>()) }
+
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = "GET MEALS") {
+        coroutineScope.launch(Dispatchers.IO) {
+            val meals = viewModel.getMeals()
+            rememberedMeals.value = meals
+
+        }
     }
+
+
     LazyColumn {
         items(rememberedMeals.value) { meal ->
             Text(text = meal.name)
--- app/src/main/java/com/example/mealsappusingcompose/meals/MealsCategoriesViewModel.kt
+++ app/src/main/java/com/example/mealsappusingcompose/meals/MealsCategoriesViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.mealsappusingcompose.meals
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.mealsappusingcompose.model.MealsRepository
+import com.example.mealsappusingcompose.model.response.MealResponse
 import com.example.mealsappusingcompose.model.response.MealsCategoriesResponse
 
 class MealsCategoriesViewModel (private val repository: MealsRepository = MealsRepository()): ViewModel() {
-    fun getMeals(successCallback: (response: MealsCategoriesResponse?) -> Unit) {
-        repository.getMeals { response ->
-            successCallback(response)
-        }
-    }
+
+    // val mealsState: MutableState<List<MealResponse>> = mutableStateOf(emptyList<MealResponse>())
+
+    suspend fun getMeals() : List<MealResponse> {
+       return repository.getMeals().categories
+   }
 }
