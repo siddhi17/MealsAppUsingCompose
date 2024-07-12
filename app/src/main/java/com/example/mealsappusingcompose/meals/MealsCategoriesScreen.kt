751e98ef6d749faceec960bcba6641c727a2f63d
siddhi17 <siddhijambhale@gmail.com>
2024-07-12 17:26:21 +0530

    Expanding items in Meals list

--- app/src/main/java/com/example/mealsappusingcompose/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/com/example/mealsappusingcompose/meals/MealsCategoriesScreen.kt
@@ -3,7 +3,12 @@ package com.example.mealsappusingcompose.meals
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,7 +20,11 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -100,6 +109,7 @@ fun MealCategory(meal: MealResponse, navigationCallback: (String) -> Unit) {
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
